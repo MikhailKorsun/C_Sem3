a996f831b3f1e1909876819079b0c2cde9bcb827
Mikhail Korsun <mr.reduction@gmail.com>
2023-05-01 21:39:59 +0300

    add [] into task 21

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿// string[] friend = new string[5];
+// string[] friend = new string[5] {"Bob", "Ann", "Rob", "Tom", "Bull"};
+// string[] friend = new string[]  {"Bob", "Ann", "Rob", "Tom", "Bull"};
+ string[] friend = {"Bob", "Ann", "Rob", "Tom", "Bull"};
+ Console.WriteLine(friend[3]);
--- Task21/Program.cs
+++ Task21/Program.cs
@@ -4,22 +4,40 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+// Console.WriteLine("Введите координаты точки А: ");
+// Console.Write("xA = ");
+// int xA = Convert.ToInt32(Console.ReadLine());
+// Console.Write("yA = ");
+// int yA = Convert.ToInt32(Console.ReadLine());
+// Console.Write("zA = ");
+// int zA = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите координаты точки B: ");
+// Console.Write("xB = ");
+// int xB = Convert.ToInt32(Console.ReadLine());
+// Console.Write("yB = ");
+// int yB = Convert.ToInt32(Console.ReadLine());
+// Console.Write("zB = ");
+// int zB = Convert.ToInt32(Console.ReadLine());
+
+int[] a = new int[3];
+int[] b = new int[3];
 Console.WriteLine("Введите координаты точки А: ");
 Console.Write("xA = ");
-int xA = Convert.ToInt32(Console.ReadLine());
+a[0] = Convert.ToInt32(Console.ReadLine());
 Console.Write("yA = ");
-int yA = Convert.ToInt32(Console.ReadLine());
+a[1] = Convert.ToInt32(Console.ReadLine());
 Console.Write("zA = ");
-int zA = Convert.ToInt32(Console.ReadLine());
+a[2] = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите координаты точки B: ");
 Console.Write("xB = ");
-int xB = Convert.ToInt32(Console.ReadLine());
+b[0] = Convert.ToInt32(Console.ReadLine());
 Console.Write("yB = ");
-int yB = Convert.ToInt32(Console.ReadLine());
+b[1] = Convert.ToInt32(Console.ReadLine());
 Console.Write("zB = ");
-int zB = Convert.ToInt32(Console.ReadLine());
+b[2] = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine($"A({xA},{yA},{zA}), B({xB},{yB},{zB})");
-double distance = Math.Round(Math.Sqrt(Math.Pow((xB-xA),2) + Math.Pow((yB-yA),2) + Math.Pow((zB-zA),2)), 2);
+Console.WriteLine($"A({a[0]},{a[1]},{a[2]}), B({b[0]},{b[1]},{b[2]})");
+double distance = Math.Round(Math.Sqrt(Math.Pow((b[0]-a[0]),2) + Math.Pow((b[1]-a[1]),2) + Math.Pow((b[2]-a[2]),2)), 2);
 Console.WriteLine($"Расстояние между точками A и B = {distance}");
