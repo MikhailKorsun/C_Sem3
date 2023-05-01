7f3fc3f9d971fffed60d52ca452fba18a1125cb4
Mikhail Korsun <mr.reduction@gmail.com>
2023-05-01 22:19:22 +0300

    Complete task 18

---
+++ Task18/Program.cs
@@ -0,0 +1,24 @@
+﻿// 18) Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.Write("Введите  номер четверти: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+switch (quarter)
+{
+    case 1:
+        Console.WriteLine("X > 0, Y > 0 ");
+        break;
+    case 2:
+        Console.WriteLine("X < 0, Y > 0 ");
+        break;
+    case 3:
+        Console.WriteLine("X < 0, Y < 0 ");
+        break;
+    case 4:
+        Console.WriteLine("X > 0, Y < 0 ");
+        break;
+    default:
+        Console.WriteLine("Введено число, которое не соответсвует номеру четверти декардовых координат. Используйте числа 1, 2, 3 или 4");
+        break;
+}
