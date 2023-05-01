1d15ebf47ad56a4141102c1d1c4b93550df832dc
Mikhail Korsun <mr.reduction@gmail.com>
2023-05-01 22:31:04 +0300

    Task1 (2D) modified

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,9 +1,21 @@
-﻿// string[] friend = new string[5];
-// string[] friend = new string[5] {"Bob", "Ann", "Rob", "Tom", "Bull"};
-// string[] friend = new string[]  {"Bob", "Ann", "Rob", "Tom", "Bull"};
- string[] friend = {"Bob", "Ann", "Rob", "Tom", "Bull"};
+﻿//21) Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09; A (7,-5); B (1,-1) -> 7,21;
 
- for (int i = 0; i < friend.Length; i++)
- {
-     Console.WriteLine(friend[i]);
- }
+int[] a = new int[2];
+int[] b = new int[2];
+Console.WriteLine("Введите координаты точки А: ");
+Console.Write("xA = ");
+a[0] = Convert.ToInt32(Console.ReadLine());
+Console.Write("yA = ");
+a[1] = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B: ");
+Console.Write("xB = ");
+b[0] = Convert.ToInt32(Console.ReadLine());
+Console.Write("yB = ");
+b[1] = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"A({a[0]},{a[1]}), B({b[0]},{b[1]})");
+
+double distance = Math.Round(Math.Sqrt(Math.Pow((b[0]-a[0]),2) + Math.Pow((b[1]-a[1]),2)), 2);
+Console.WriteLine($"Расстояние между точками A и B = {distance}");
