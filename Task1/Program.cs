b813a4d0019340f5d93e8f7c4740f8f8c5883e15
Mikhail Korsun <mr.reduction@gmail.com>
2023-05-01 22:06:49 +0300

    Task 17 done

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,4 +2,8 @@
 // string[] friend = new string[5] {"Bob", "Ann", "Rob", "Tom", "Bull"};
 // string[] friend = new string[]  {"Bob", "Ann", "Rob", "Tom", "Bull"};
  string[] friend = {"Bob", "Ann", "Rob", "Tom", "Bull"};
- Console.WriteLine(friend[3]);
+
+ for (int i = 0; i < friend.Length; i++)
+ {
+     Console.WriteLine(friend[i]);
+ }
